2ece3f7adf0839041cc9ea457d444c42725ad536
breadbeard <dvantuyl@gmail.com>
2016-09-30 18:31:13 -0700

    Update rehearsal title

--- app/models/rehearsal.rb
+++ app/models/rehearsal.rb
@@ -1,8 +1,15 @@
 class Rehearsal < Event
 
   enum status: {lets_play: 0, canceled: 2}
-  
+
   belongs_to :rehearsal_pic, class_name: "Member"
 
+  def title
+    "#{self.start_time.strftime("%^a")} Rehearsal @ #{self.location}"
+  end
+
+  def to_s
+    self.title
+  end
 
 end
